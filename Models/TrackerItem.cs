e87f4e5399cdacd1c080ae37c2b9c3c65276b4ed
Steven Carleton <scarleton@700credit.com>
2025-12-18 14:06:00 -0500

    Add form and model with validation. Add lineitem component

---
+++ Models/TrackerItem.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorTracker.Models;
+
+public class TrackerItem
+{
+    public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "Name is required")]
+    public string Name { get; set; } = default!;
+
+    public string Description { get; set; } = default!;
+
+    public bool IsComplete { get; set; } = default!;
+}
